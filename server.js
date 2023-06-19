6b032333cf14ad3fe5a3509f82efa5af61b3e90f
utsavchab <21je1011@iitism.ac.in>
2023-06-20 00:02:58 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+
+app.get("/" , function(req,res){
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+app.listen(process.env.PORT || 3000 , function(){
+    console.log('Server Live at Server 3000')
+})
